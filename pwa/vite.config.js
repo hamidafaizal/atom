e9a9ac445701da14702ee58e37ae0f79191d0bd5
hamidafaizal <hamidafaizalamin0@gmail.com>
2025-08-04 20:27:14 +0700

    1 tes

--- pwa/vite.config.js
+++ pwa/vite.config.js
@@ -1,7 +1,36 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+    import { defineConfig } from 'vite'
+    import react from '@vitejs/plugin-react'
+    import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+    // https://vite.dev/config/
+    export default defineConfig({
+      plugins: [
+        react(),
+        VitePWA({
+          registerType: 'autoUpdate',
+          // Konfigurasi manifest PWA
+          manifest: {
+            name: 'Aplikasi Absensi PWA',
+            short_name: 'Absensi',
+            description: 'Aplikasi absensi karyawan yang dapat diinstal.',
+            theme_color: '#1a202c',
+            background_color: '#111827',
+            display: 'standalone',
+            start_url: '/',
+            icons: [
+              {
+                src: 'absensi-icon-192.png',
+                sizes: '192x192',
+                type: 'image/png'
+              },
+              {
+                src: 'absensi-icon-512.png',
+                sizes: '512x512',
+                type: 'image/png'
+              }
+            ]
+          }
+        })
+      ],
+    })
+    
